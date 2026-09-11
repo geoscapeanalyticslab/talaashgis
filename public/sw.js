c231dfe702097c6288abc871a0dddde9f2d8eca3
Muhammad Mehdi <geomehdi12@gmail.com>
2026-09-11 12:27:17 +0500

    Full-width layout, Times New Roman, bigger GRIPS title, cleaner text

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'grips-shell-v2';
+const CACHE = 'grips-shell-v3';
 const SHELL_FILES = ['index.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', event => {
