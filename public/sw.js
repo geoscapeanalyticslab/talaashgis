8fef88484000b080f4883d0f67eeadc0fbbae4a5
Muhammad Mehdi <geomehdi12@gmail.com>
2026-09-11 15:45:55 +0500

    Reset theme to light default (rename theme storage key)

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'grips-shell-v6';
+const CACHE = 'grips-shell-v7';
 const SHELL_FILES = ['index.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', event => {
