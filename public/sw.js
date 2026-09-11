9c4aac99dcd56b33cbb43294643025dae0081fe5
Muhammad Mehdi <geomehdi12@gmail.com>
2026-09-11 12:13:35 +0500

    Header: full name in Times New Roman

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'grips-shell-v1';
+const CACHE = 'grips-shell-v2';
 const SHELL_FILES = ['index.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', event => {
