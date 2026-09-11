f9bb84e3420257a360bb0e0c12ddd2dd64a60bb6
Muhammad Mehdi <geomehdi12@gmail.com>
2026-09-11 14:42:56 +0500

    Move tabs to top-right of header

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'grips-shell-v3';
+const CACHE = 'grips-shell-v4';
 const SHELL_FILES = ['index.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', event => {
