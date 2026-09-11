d46956f458fcc73c8063bfd645c51cd634855726
Muhammad Mehdi <geomehdi12@gmail.com>
2026-09-11 15:41:52 +0500

    Link GSAL lab site in footer

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'grips-shell-v5';
+const CACHE = 'grips-shell-v6';
 const SHELL_FILES = ['index.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', event => {
